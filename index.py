25a6ae5e6f5e8c107e3581601ff8285424db2331
samridhikansal <kansal.samridhi@gmail.com>
2024-12-23 23:10:00 -0700

    the computer ask the user to select a number. the user is given 6 choices. If the user guessed it right, message is displayed. after all the turns are exhausted, the computer tell the right number

--- index.py
+++ index.py
@@ -1,5 +1,16 @@
 import random
-import string 
 
-name = input ("Enter your name:")
-print (name)
+myChoice = random.randint(1,10)
+
+for i in range (0,6):
+    userChoice = int(input("Guess the number: "))
+    if userChoice == myChoice:
+        print("you guessed it right")
+        break
+    else:
+        if i < 5:
+         print("Try Again")
+        else:
+         print ("Sorry you have exhausted all your chances")
+         print ("The number was ", myChoice)
+
